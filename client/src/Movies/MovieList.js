c52e15ce0d310f7739807036b4bed0813e8cd169
Matt Basile <matthewbasile2@gmail.com>
2019-01-22 14:50:33 -0500

    Refactored MovieCard Component for a more DRY approach.

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -9,7 +9,6 @@ export default class MovieList extends Component {
       movies: []
     };
   }
-
   componentDidMount() {
     axios
       .get('http://localhost:5000/api/movies')
@@ -25,33 +24,14 @@ export default class MovieList extends Component {
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
+          <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
     );
   }
 }
 
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars} = movie;
-  return (
-    <Link to={`/movie/${movie.id}`}>
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-    </Link>
-  );
-}
+// function MovieDetails({ movie }) {
+  
+  
+// }
